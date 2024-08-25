58d2337746344677663bad62712a4a60c648f61f
Diego Henrique de Oliveira Madero <60300499+diegogit03@users.noreply.github.com>
2024-08-25 00:48:28 -0300

    first release

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,29 +1,77 @@
 @extends('layout')
 
 @section('content')
-    <div class="hero">
-        <div class="text-white text-center">
-            <h1>Diego Henrique</h1>
-            <h2 class="my-3">Desenvolvedor de Sofware</h2>
-            <button class="btn btn-lg btn-outline-primary">Entrar em Contato</button>
+    <div class="container py-4">
+        <div class="row">
+            <div class="col-md-5 col-12">
+                <img style="width: 100%; height: 20rem;border-radius: 2rem" src="https://cursinhoparamedicina.com.br/wp-content/uploads/2022/10/Paisagem-1.jpg" alt="">
+            </div>
+            <div class="col-md-7 col-12 py-2 d-flex flex-column justify-content-center">
+                <h1 class="mb-2" style="font-size: 3rem">Olá, Sou o Diego!</h1>
+                <p class="fs-4">Eu construo e desenvolvo sofware de qualidade, minha principal especialidade são aplicações backend, design de API's e integrações</p>
+                <div class="mt-3">
+                    <button class="btn btn-lg btn-primary" style="border-radius: 1.5rem">Entrar em contato</button>
+                    <button class="btn btn-secondary">Ver portfolio</button>
+                </div>
+            </div>
         </div>
     </div>
 
     <section class="container" style="padding: 5rem 0;">
-        <h3 class="text-center mb-4">Portfólio</h3>
+        <h3 class="text-center mb-4">Projetos</h3>
         <div class="row">
             @foreach ($projects as $project)
-                <div class="col-12 col-md-6 col-lg-4">
-                    <div class="card">
-                        <img src="{{ asset('/storage/' . $project->image_filename) }}" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">{{ $project->title }}</h5>
-                            <p class="card-text">{{ $project->description }}</p>
-                            <a href="#" class="btn btn-primary">Saiba mais</a>
-                        </div>
-                    </div>
+                <div class="col-12 col-md-6 col-lg-4 d-flex justify-content-center align-items-center flex-column">
+                    <a href="{{ $project->link }}" class="fade-text" target="_blank">
+                        <img class="w-100 " src="{{ asset('/storage/' . $project->image_filename) }}" alt="">
+                        <p class="text-overlay">{{ $project->title }}</p>
+                    </a>
                 </div>
                 @endforeach
         </div>
     </section>
+
+    <!--
+    <section class="container" style="padding-bottom: 5rem">
+        <h3 class="text-center mb-4">Tecnologias</h3>
+        <div class="row">
+            <div class="col-2">
+                <div class="bg-primary py-2 px-3 text-white text-center" style="border-radius: 2rem; height: 14rem">
+                    <img class="w-100" src="https://cdn.iconscout.com/icon/free/png-256/free-php-71-458168.png?f=webp&w=256" alt="">
+                    <p class="fw-bold fs-3">PHP</p>
+                </div>
+            </div>
+            <div class="col-2">
+                <div class="bg-primary py-2 px-3 text-white text-center" style="border-radius: 2rem; height: 14rem">
+                    <img class="w-100" src="https://cdn.iconscout.com/icon/free/png-256/free-php-71-458168.png?f=webp&w=256" alt="">
+                    <p class="fw-bold fs-3">PHP</p>
+                </div>
+            </div>
+            <div class="col-2">
+                <div class="bg-primary py-2 px-3 text-white text-center" style="border-radius: 2rem; height: 14rem">
+                    <img class="w-100" src="https://cdn.iconscout.com/icon/free/png-256/free-php-71-458168.png?f=webp&w=256" alt="">
+                    <p class="fw-bold fs-3">PHP</p>
+                </div>
+            </div>
+            <div class="col-2">
+                <div class="bg-primary py-2 px-3 text-white text-center" style="border-radius: 2rem; height: 14rem">
+                    <img class="w-100" src="https://cdn.iconscout.com/icon/free/png-256/free-php-71-458168.png?f=webp&w=256" alt="">
+                    <p class="fw-bold fs-3">PHP</p>
+                </div>
+            </div>
+            <div class="col-2">
+                <div class="bg-primary py-2 px-3 text-white text-center" style="border-radius: 2rem; height: 14rem">
+                    <img class="w-100" src="https://cdn.iconscout.com/icon/free/png-256/free-php-71-458168.png?f=webp&w=256" alt="">
+                    <p class="fw-bold fs-3">PHP</p>
+                </div>
+            </div>
+            <div class="col-2">
+                <div class="bg-primary py-2 px-3 text-white text-center" style="border-radius: 2rem; height: 14rem">
+                    <img class="w-100" src="https://cdn.iconscout.com/icon/free/png-256/free-php-71-458168.png?f=webp&w=256" alt="">
+                    <p class="fw-bold fs-3">PHP</p>
+                </div>
+            </div>
+        </div>
+    </section>
+-->
 @endsection
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -8,19 +8,21 @@
     @vite(['resources/css/app.scss', 'resources/js/app.js'])
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg bg-primary  py-3">
-        <div class="container">
-          <a class="navbar-brand" href="{{ route('home') }}">DH</a>
+    <nav class="navbar navbar-expand-lg bg-none  py-3" style="margin-bottom: 3rem">
+        <div class="container d-flex align-items-center">
+          <a class="navbar-brand"  style="width: 1.5rem" href="{{ route('home') }}">
+            <img class="w-100" src="{{ asset('logo.png') }}" alt="">
+          </a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarNavDropdown">
             <ul class="navbar-nav ms-auto">
               <li class="nav-item">
-                <a class="nav-link active text-white fw-bold" aria-current="page" href="{{ route('home') }}">Home</a>
+                <a class="nav-link {{ request()->routeIs('home') ? 'active' : '' }} fw-bold" aria-current="page" href="{{ route('home') }}">Home</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link text-white fw-bold" href="{{ route('blog') }}">Blog</a>
+                <a class="nav-link {{ request()->routeIs('blog') || request()->routeIs('blog.article') ? 'active' : '' }} fw-bold" href="{{ route('blog') }}">Blog</a>
               </li>
             </ul>
           </div>
@@ -34,8 +36,10 @@
             <div class="row">
                 <div class="col-6">
                     <h4>Redes Sociais</h4>
-                    <a class="btn btn-outline-info"><i class="bi bi-github"></i></a>
-                    <a class="btn btn-outline-info"><i class="bi bi-instagram"></i></a>
+                    <div class="d-flex social-links" style="gap: 1rem">
+                        <a href="https://www.github.com/diegogit03" class="text-white fs-2" target="_blank"><i class="bi bi-github"></i></a>
+                        <a class="text-white fs-2" target="_blank"><i class="bi bi-instagram"></i></a>
+                    </div>
                 </div>
                 <div class="col-6">
                     <h4>Contatos</h4>
@@ -44,7 +48,7 @@
                 </div>
             </div>
             <div class="text-center pt-4">
-                Desenvolvido por <span class="text-primary">Diego Henrique</span>.
+                Desenvolvido por <span class="fw-bold">Diego Henrique</span>.
             </div>
         </div>
     </footer>
